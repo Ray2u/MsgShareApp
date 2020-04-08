9a8a2c33a7feebd248e920e1a2740f5b6151d3e4
Ray2u <RayDowsett@gmail.com>
2020-04-08 16:19:14 +1000

    Section 3

---
+++ app/src/main/java/com/smartherd/msgshareapp/Model.kt
@@ -0,0 +1,39 @@
+package com.smartherd.msgshareapp
+
+data class Hobby(var title: String)
+
+object Supplier {
+
+    val hobbies = listOf<Hobby>(
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Walking"),
+        Hobby("Sleeping"),
+        Hobby("Gaming"),
+        Hobby("Talking"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Walking"),
+        Hobby("Sleeping"),
+        Hobby("Gaming"),
+        Hobby("Talking"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Walking"),
+        Hobby("Sleeping"),
+        Hobby("Gaming"),
+        Hobby("Talking"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Walking"),
+        Hobby("Sleeping"),
+        Hobby("Gaming"),
+        Hobby("Talking"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Walking"),
+        Hobby("Sleeping"),
+        Hobby("Gaming"),
+        Hobby("Talking")
+    )
+}
